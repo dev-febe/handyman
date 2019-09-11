819e19317c8d00eacfc04ec593045af381cc5e99
Ujjwal Wahi <w.ujjwal@gmail.com>
2019-09-11 15:03:59 +0530

    Fix: Subcategory search

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -15,18 +15,15 @@ class CategoryController extends Controller
      */
     public function index(Request $request)
     {
-        if($request->category_id) {
-            $categories = Category::where('parent_id', $request->category_id);
-	} else if($request->search_subcategory) {
-	    // search in subcategory
-	    $categories = Category::where('title', 'like', "%" . $request->search_subcategory . "%")->whereNotNull('parent_id');
-	} else {
-            $categories = Category::whereNull('parent_id');
-        }
+        $categories = Category::whereRaw("1=1");
 
         // search
-        if($request->search) {
+        if ($request->search) {
             $categories = $categories->where('title', 'like', "%" . $request->search . "%");
+        } else if ($request->category_id) {
+            $categories = $categories->where('parent_id', $request->category_id);
+        } else {
+            $categories = $categories->whereNull('parent_id');
         }
 
         $categories = $categories->orderBy('title', 'desc')->paginate(50);
