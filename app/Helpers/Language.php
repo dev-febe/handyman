839ff087887082bb83be3f20ad674b42fec49636
Ujjwal Wahi <w.ujjwal@gmail.com>
2019-09-26 17:09:38 +0530

    Fix: notification language

--- app/Helpers/Language.php
+++ app/Helpers/Language.php
@@ -2,61 +2,52 @@
 
 namespace App\Helpers;
 
+use App\Models\Setting;
+
 class Language
 {
     private $languageCode;
 
-    private $dictionary = [
-        "en" => [
-            "appointment_new_title" => "New Appointment",
-            "appointment_new_body" => "You have recieved new appointment for service",
-            "appointment_rescheduled_title" => "Appointment Rescheduled",
-            "appointment_rescheduled_body" => "Your appointment with provider is rescheduled",
-            "appointment_cancelled_title" => "Appointment Cancelled",
-            "appointment_cancelled_body" => "Client has cancelled the appointment",
-            "appointment_rejected_title" => "Appointment Rejected",
-            "appointment_rejected_body" => "Provider has rejected the appointment",
-            "appointment_accepted_title" => "Appointment Accepted",
-            "appointment_accepted_body" => "Provider has accepted the appointment",
-            "appointment_ongoing_title" => "Appointment Started",
-            "appointment_ongoing_body" => "Provider has started the appointment",
-            "appointment_complete_title" => "Appointment Complete",
-            "appointment_complete_body" => "Your appointment with provider is complete",
-        ],
-        "es" => [
-            "bus" => "Bus",
-            "from" => "desde",
-            "at" => "a",
-            "picked_from_school" => "Se ha recogido del Colegio",
-            "picked_from_home" => "Se ha recogido de la Casa",
-            "dropped_at_home" => "Llego a la Casa",
-            "dropped_at_school" => "Llego al Colegio",
-            "pickup" => "Recoger",
-            "dropped" => "Entregar",
-            "picked" => "Recogido"
-        ],
-        "ar" => [
-            "bus" => "نقل",
-            "from" => "من",
-            "at" => "عند",
-            "picked_from_school" => "تم احضاره من المدرسة",
-            "picked_from_home" => "تم احضاره من المدرسة",
-            "dropped_at_home" => "تم ايصاله للمنزل",
-            "dropped_at_school" => "تم ايصاله للمدرسة",
-            "pickup" => "نقطة التقاء",
-            "dropped" => "نقطة وصول",
-            "picked" => "تم احضاره"
-        ],
-    ];
+    public $dictionary = [];
+    public $fallback;
 
     public function __construct($languageCode)
     {
         $this->languageCode = $languageCode ? $languageCode : "en";
+        $this->fallback = json_decode(json_encode([
+            "en" => [
+                "appointment_new_title" => "New Appointment",
+                "appointment_new_body" => "You have recieved new appointment for service",
+                "appointment_rescheduled_title" => "Appointment Rescheduled",
+                "appointment_rescheduled_body" => "Your appointment with provider is rescheduled",
+                "appointment_cancelled_title" => "Appointment Cancelled",
+                "appointment_cancelled_body" => "Client has cancelled the appointment",
+                "appointment_rejected_title" => "Appointment Rejected",
+                "appointment_rejected_body" => "Provider has rejected the appointment",
+                "appointment_accepted_title" => "Appointment Accepted",
+                "appointment_accepted_body" => "Provider has accepted the appointment",
+                "appointment_ongoing_title" => "Appointment Started",
+                "appointment_ongoing_body" => "Provider has started the appointment",
+                "appointment_complete_title" => "Appointment Complete",
+                "appointment_complete_body" => "Your appointment with provider is complete"
+            ]
+        ]));
+
+        try {
+            $this->dictionary = json_decode(Setting::where('key', 'language')->get()->first()->value);
+        } catch (\Exception $exception) {
+            $this->dictionary = $this->fallback;
+        }
     }
 
     public function get($key)
     {
-        $languageCode = array_key_exists($this->languageCode, $this->dictionary) ? $this->languageCode : "en";
-        return $this->dictionary[$languageCode][$key];
+        $languageCode = $this->languageCode;
+        $languageCode = isset($this->dictionary->$languageCode) ? $languageCode : "en";
+        try {
+            return $this->dictionary->$languageCode->$key;
+        } catch (\Exception $exception) {
+            return $this->fallback->en->$key;
+        }
     }
 }
